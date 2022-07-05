78512138e4a5dcb42a98b59eac368e161e9a4328
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2022-07-05 11:17:55 +0200

    cleanup

--- src/test/java/jenkins/plugins/foldericon/utils/TestUtils.java
+++ src/test/java/jenkins/plugins/foldericon/utils/TestUtils.java
@@ -1,32 +1,3 @@
-/* ******************************************************************
- * Copyright (c) 2003-2022 by PROSTEP AG                             *
- * All rights reserved                                               *
- *                                                                   *
- *   ___  ___  ___  _  _  ___  __   _    _                           *
- *  | _ || _ || __|| \| || _ || _ \| \  / |                          *
- *  ||_||| /_|| _/ |  ' || /_|||_ ||  ''  |                          *
- *  |___||_|  |___||_|\_||_|  |__ /|_|\/|_|                          *
- *                                                                   *
- * This software is furnished under a license and may be used and    *
- * copied only in accordance with the terms of such license and      *
- * with the inclusion of the above copyright notice. This            *
- * software or any other copies thereof may not be provided or       *
- * otherwise made available to a third person. No title to and       *
- * ownership of the software is hereby transferred.                  *
- *                                                                   *
- * The information in this software is subject to change without     *
- * notice and should not be construed as a commitment by ProSTEP     *
- *                                                                   *
- * The PROSTEP AG assumes only responsibility defined in a contract  *
- * and no responsibility for the use or reliability of its software  *
- * on equipment which is not supplied by the PROSTEP AG.             *
- *                                                                   *
- *********************************************************************/
-
-//
-// Author      : dkraemer
-// Start       : 05.07.2022
-//
 package jenkins.plugins.foldericon.utils;
 
 import static org.junit.Assert.assertFalse;
@@ -42,7 +13,9 @@ import org.mockito.Mockito;
 import com.cloudbees.hudson.plugins.folder.FolderIcon;
 
 /**
- * @author dkraemer
+ * Common utils for tests.
+ * 
+ * @author strangelookingnerd
  *
  */
 public final class TestUtils {
