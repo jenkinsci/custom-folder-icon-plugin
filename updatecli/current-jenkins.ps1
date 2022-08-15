8066db7cb191a1a28aa5e28a567c827f190b2ad4
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2022-08-15 13:32:33 +0200

    Update current-jenkins.ps1

--- updatecli/current-jenkins.ps1
+++ updatecli/current-jenkins.ps1
@@ -5,6 +5,8 @@ param(
 
 $regex = "\[ platform: '\w+', jdk: '\d{2}', jenkins: '(?<version>\d+.\d+.\d)' \]"
 
+Write-Output "2.363"
+
 if((Get-Content $JenkinsfilePath) -match $regex) {
   Write-Output $Matches.version
 }
