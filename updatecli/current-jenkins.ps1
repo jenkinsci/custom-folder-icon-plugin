e925a028544f91f8ee5905fd0c845ef8109a7da8
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2022-08-15 17:26:44 +0200

    Use Pattern Matching instead

--- updatecli/current-jenkins.ps1
+++
@@ -1,6 +0,0 @@
-param(
-  [Parameter(Position = 0)]
-  [string] $JenkinsfilePath
-)
-
-(Get-Content $JenkinsfilePath) | Select-String -Pattern "\[ platform: 'linux', jdk: '17', jenkins: '(.+)' \]" | %{$_.Matches.Groups[1].value}
