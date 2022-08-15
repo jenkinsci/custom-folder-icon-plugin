7fea051648c4a260fe425a4eb278e014eae62f00
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2022-08-15 15:41:55 +0200

    Update update-jenkins.ps1

--- updatecli/update-jenkins.ps1
+++ updatecli/update-jenkins.ps1
@@ -18,6 +18,9 @@ if ($null -ne $CurrentVersion -and $NewVersion -gt $CurrentVersion) {
 
 if ($changed) {
   Write-Output "$NewVersion"
-  $NewContent = (Get-Content $JenkinsfilePath) -replace $CurrentVersion, $NewVersion
-  Set-Content -Path $JenkinsfilePath -Value $NewContent
+  if ($ENV:DRY_RUN -eq $false) {
+    $NewContent = (Get-Content $JenkinsfilePath) -replace $CurrentVersion, $NewVersion
+    Set-Content -Path $JenkinsfilePath -Value $NewContent
+    Write-Output "CHANGED!"
+  }
 }
