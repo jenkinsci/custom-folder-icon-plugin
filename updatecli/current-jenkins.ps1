2a9d9510a3d2ae493c539a4696736943a9ff5311
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2022-08-15 13:48:51 +0200

    Update current-jenkins.ps1

--- updatecli/current-jenkins.ps1
+++ updatecli/current-jenkins.ps1
@@ -5,4 +5,6 @@ param(
 
 if((Get-Content $JenkinsfilePath) -match "\[ platform: '\w+', jdk: '\d{2}', jenkins: '(?<version>\d+.\d+.\d)' \]") {
   Write-Output $Matches.version
+} else {
+  Write-Output "Why is it not matching!?"
 }
