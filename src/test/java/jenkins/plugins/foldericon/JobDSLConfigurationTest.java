e129711616e242e384e3365b2e174c20127583dc
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2024-09-05 12:12:04 +0200

    Migration to Jakarta EE

--- src/test/java/jenkins/plugins/foldericon/JobDSLConfigurationTest.java
+++ src/test/java/jenkins/plugins/foldericon/JobDSLConfigurationTest.java
@@ -1,27 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright (c) 2024 strangelookingnerd
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package jenkins.plugins.foldericon;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.cloudbees.hudson.plugins.folder.Folder;
 import com.cloudbees.hudson.plugins.folder.FolderIcon;
 import hudson.FilePath;
-import hudson.model.*;
+import hudson.model.BallColor;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Item;
+import hudson.model.Result;
 import java.io.File;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -45,8 +25,6 @@ import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
 /**
  * Test to configure folders via Job DSL plugin.
- *
- * @author strangelookingnerd
  */
 @WithJenkins
 class JobDSLConfigurationTest {
